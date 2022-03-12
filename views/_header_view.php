083713bdea8ee27de033ac4c8b755285e5a6ce18
satoru <40satoruoka10@gmail.com>
2022-03-12 18:39:13 +0900

    views partial

---
+++ views/_header_view.php
@@ -0,0 +1,9 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a href="/" class="navbar-brand ">
+                <strong>E-Learning</strong>
+            </a>
+        </div>
+    </nav>
+</header>
